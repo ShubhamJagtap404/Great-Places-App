14e6591392b81846b8f54b2ff3813cd745fb2963
ShubhamJagtap404 <51310448+ShubhamJagtap404@users.noreply.github.com>
2021-02-15 15:50:55 +0530

    Create places-list-provider

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:GreatPlaces/providers/great_places.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './screens/places_list_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child:MaterialApp(
+      title: 'Great Places',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: PlacesListScreen(),
+    )
+    );
+    
+  }
+}
+
